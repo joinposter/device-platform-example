d9ed7dc111d3b4dc9e5affd9484d9c7e641f819a
Demchenko Dmitry <demchenko20021@gmail.com>
2019-10-08 14:00:29 +0300

    Updated library & test example

--- app/src/main/java/com/example/posterpos/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/posterpos/myapplication/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.posterpos.myapplication
 import android.os.Bundle
 import android.os.StrictMode
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
 import com.joinposter.transport.PosterTransport
 import com.joinposter.transport.server.PosterDevice
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.SendBtn
+
+private const val TAG = "PosterTransport"
+private const val APP_ID = "272"
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,16 +22,36 @@ class MainActivity : AppCompatActivity() {
         StrictMode.setThreadPolicy(policy)
 
         SendBtn.setOnClickListener {
-            PosterTransport.devices.forEach { it.sendMessage("{\"text\":\"Hello!\"}") }
+            val connected = PosterTransport.connectedDevices
+            connected.forEach {
+                it.sendMessage("{\"text\":\"Hello!\"}")
+            }
         }
 
-        PosterTransport.init(applicationContext, "272")
-        //We want to track what terminal sends to us
-        PosterTransport.onMessage = this.onMessage
-        //Make current device visible to Poster terminal
+        with(PosterTransport) {
+            onMessage = this@MainActivity.onMessage
+            onClose = { code, info, initByRemote ->
+                Log.d(TAG, "Closed connection: $code $info $initByRemote")
+            }
+            onError = { error ->
+                Log.d(TAG, "Error: ${error.message ?: error.toString()}")
+            }
+            onFinishedInit = {
+                Log.d(TAG, "Library is ready")
+            }
+            onOpen = {
+                hashMapOf("simple" to "test")
+            }
+
+            // Initializing library
+            init(applicationContext, APP_ID)
+        }
+
+        // Make current device visible to Poster terminals
         PosterTransport.start()
     }
 
+    // Tracking what terminal sends to our app
     private val onMessage: (device: PosterDevice, message: String) -> Unit = { device, message ->
         runOnUiThread {
             Toast.makeText(this, "Message: $message\nFrom: ${device.ip}", Toast.LENGTH_SHORT).show()
